34e0ed98790e381d735aef164371f11d4b7b7d03
meghanamuthukumar14 <meghana.muthukumar@thoughtclan.com>
2021-09-07 20:21:51 +0530

    committed

---
+++ src/com/tc/training/java/prime/PrimeFinder.java
@@ -0,0 +1,26 @@
+package com.tc.training.java.prime;
+
+public class PrimeFinder {
+    public static void main(String[] args) {
+        int i, j, N, flag;
+        N = 100;
+        System.out.println("\nPrime numbers from 1 to 100 are: ");
+        for (i = 1; i <= N; i++)
+        {
+            if (i == 1 || i == 0)
+                continue;
+            flag = 1;
+            for (j = 2; j <= i / 2; ++j)
+            {
+                if (i % j == 0)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+
+            if (flag == 1)
+                System.out.print(i + " ");
+        }
+    }
+}
